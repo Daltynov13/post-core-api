b606260232c3656665baaedc2e06344fff79640a
daltynov <DAltynov@beeline.kz>
2022-06-26 10:50:22 +0600

    add eureka client

--- src/main/java/kz/dar/academy/backend/postcoreapi/controller/PostController.java
+++ src/main/java/kz/dar/academy/backend/postcoreapi/controller/PostController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import kz.dar.academy.backend.postcoreapi.model.PostModel;
 import kz.dar.academy.backend.postcoreapi.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,9 +22,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/post")
 public class PostController {
 
-
+  @Autowired
+  private Environment env;
   @Autowired
   private PostService postService;
+
+  @GetMapping("/check")
+  public String checkPostCoreApi() {
+    return "Service is WORKING at " + env.getProperty("local.server.port");
+  }
 
   @PostMapping
   public ResponseEntity<String> createPost(@Valid @RequestBody PostModel post) {
